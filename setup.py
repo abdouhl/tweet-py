45054e1d490fbec488fcdc37146ffc9c0481c786
abdou_hl <abderrahmane.36.hellal@gmail.com>
2023-01-21 13:59:42 +0100

    json to dict

--- setup.py
+++ setup.py
@@ -8,8 +8,13 @@ setup(
 	name='tweetpy',
 	packages=find_packages(),
 	author_email="<abderrahmane@elhellal.com>",
-	url="https://github.com/abdouhl/tweetpy/",
-	version='0.1.5',
+	url="https://tweetpy.elhellal.com/",
+	project_urls = {
+	  'Homepage': 'https://tweetpy.elhellal.com/',
+	  'Source': 'https://github.com/abdouhl/tweetpy/',
+	  'Documentation':'https://tweetpy.elhellal.com#doc'
+	},
+	version='1.0.1',
 	description='Python package that allows to access the Tweet data without API',
 	long_description_content_type="text/markdown",
 	long_description=long_description,
