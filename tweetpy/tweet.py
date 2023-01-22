f899312e2d1d1a63ae8d199d49f174310e2b3305
abdou_hl <abderrahmane.36.hellal@gmail.com>
2023-01-22 20:03:42 +0100

    change create_at to datetime.datetime

--- tweetpy/tweet.py
+++ tweetpy/tweet.py
@@ -7,6 +7,7 @@ class Tweet:
 		except:
 			self.__data=dict()
 		self.__dict__.update(self.__data)
+		self.created_at = datetime.strptime(self.created_at,'%Y-%m-%dT%H:%M:%S.%fZ')
 	
 	def __str__(self):
 		return f"tweet id: {self.id}"
